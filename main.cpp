5452f93b21427bcdb3cb5a63cc28cde5a9a05efe
nmorales9 <nmorales9@mail.csuchico.edu>
2018-03-13 11:27:53 -0700

    whaty this description does

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
+// Author: Nysa Morales
 #include<iostream>
 #include<string>
 using namespace std;
 
+const int LIST_SIZE = 5;
+
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[LIST_SIZE]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        int index;
+        string item;
+        index = 0;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+        do {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
-  return 0;
+                if (input == 'A' || input == 'a')
+                {
+                        cout << "What is the item? \n";
+                        cin >> item;
+
+                        if (index <5)
+                        {
+                                list[index] = item;
+                                index++;
+                        }
+                        else 
+                        {
+                                cout << "You'll need a bigger list!" << endl;
+                        }
+                }
+        }
+        while (input != 'Q' && input != 'q');
+        return 0;
 }
